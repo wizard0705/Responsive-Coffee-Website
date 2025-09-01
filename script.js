cfad81f48e71f98512d6fc0886e5c3edc459c0db
RISHAB DAS <imrishab0705@gmail.com>
2025-09-01 15:39:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const navLinks = document.querySelectorAll('.nav-menu .nav-link');
+const menuCloseButton = document.querySelector("#menu-close-button")
+const menuOpenButton = document.querySelector("#menu-open-button")
+
+
+menuOpenButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu")
+})
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click()
+)
+
+navLinks.forEach(link=>{
+  link.addEventListener("click", ()=>  menuOpenButton.click())
+})
+
+
+//swiper
+const swiper = new Swiper('.slider-wrapper', {
+
+  loop: true,
+  grabCursor:true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets:true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+ 
+
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
+  }
+});
